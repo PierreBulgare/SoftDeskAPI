f2060b37a68cfb9ed88b98876c64dc5ed50cb0ce
Pierre BULGARE <pierre.bulgare@gmail.com>
2025-01-31 00:10:25 +0100

    Mise à jours des serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,38 +1,68 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework.serializers import (
+    ModelSerializer, SerializerMethodField,
+    CharField, PrimaryKeyRelatedField
+)
 
 from .models import User, Contributor, Project, Issue, Comment
 
+
 class UserSerializer(ModelSerializer):
+    password = CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'age', 'can_be_contacted', 'can_data_be_shared', 'created_time']
+        extra_kwargs = {'created_time': {'read_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
 
 class ContributorSerializer(ModelSerializer):
-    user = UserSerializer()
+    user = PrimaryKeyRelatedField(queryset=User.objects.all())
+    project = PrimaryKeyRelatedField(queryset=Project.objects.all())
+
     class Meta:
         model = Contributor
-        fields = '__all__'
+        fields = ['id', 'user', 'project', 'created_time']
+        extra_kwargs = {'created_time': {'read_only': True}}
+
 
 class ProjectSerializer(ModelSerializer):
-    author = ContributorSerializer()
-    contributors = ContributorSerializer(many=True)
+    author = PrimaryKeyRelatedField(queryset=User.objects.all())
+    contributors = ContributorSerializer(many=True, read_only=True)
+
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'project_type', 'author', 'contributors', 'created_time']
+        extra_kwargs = {'created_time': {'read_only': True}}
+
+    def get_issues(self, obj):
+        return IssueSerializer(obj.issues, many=True).data
+
 
 class IssueSerializer(ModelSerializer):
-    project = ProjectSerializer()
-    author = ContributorSerializer()
+    project = PrimaryKeyRelatedField(queryset=Project.objects.all())
+    author = PrimaryKeyRelatedField(queryset=Contributor.objects.all())
     comments = SerializerMethodField()
+
     class Meta:
         model = Issue
-        fields = '__all__'
+        fields =['id', 'title', 'description', 'priority', 'balise', 'status', 'project', 'author', 'comments', 'created_time']
+        extra_kwargs = {'created_time': {'read_only': True}}
 
     def get_comments(self, obj):
         return CommentSerializer(obj.comments, many=True).data
     
 class CommentSerializer(ModelSerializer):
-    author = ContributorSerializer()
+    author = PrimaryKeyRelatedField(queryset=Contributor.objects.all())
+    issue = PrimaryKeyRelatedField(queryset=Issue.objects.all())
+
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['id', 'description', 'issue', 'author', 'created_time']
+        extra_kwargs = {'created_time': {'read_only': True}}
